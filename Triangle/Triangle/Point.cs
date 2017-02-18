f7055b064efb3e1ee15e7d319e11517bd0abb053
MaryGamer <marygamer174@gmail.com>
2017-02-18 22:16:52 +0500

    Up Point

--- Triangle/Triangle/Point.cs
+++ Triangle/Triangle/Point.cs
@@ -8,13 +8,28 @@ namespace Triangle
 {
     class Point
     {
-        public readonly double x; //or int?
-        public readonly double y; //or int?
-
-        public Point(double x, double y)
+        public double x             //or int?
         {
-            this.x = x;
-            this.y = y;
+            get { return x; }
+            set { if (value > 0 && value < 1024) x = value; }
+        }
+
+        public double y             //or int?
+        {
+            get { return y; }
+            set { if (value > 0 && value < 768) y = value; }
+        }
+
+        public Point(double x_val, double y_val) //constructor 1
+        {
+            this.x = x_val;
+            this.y = y_val;
+        }
+
+        public Point() // constructor 2 (по умолчанию)
+        {
+            this.x = 1;
+            this.y = 1;
         }
     }
 }
